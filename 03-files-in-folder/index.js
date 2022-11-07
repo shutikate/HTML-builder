ad916d482788573dd0748e3ff5a2e1bf07b025e6
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2022-11-07 14:13:40 +0100

    feat: add error handler

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,16 +5,19 @@ const { stdout } = process;
 const folderPath = path.join(__dirname, 'secret-folder');
 
 async function outputElements () {
-  const folderContent = await fs.promises.readdir(folderPath, {withFileTypes: true});
-  for (const elem of folderContent) {
-    if (!elem.isDirectory()) {
-        const filePath = path.join(folderPath, elem.name);
-        const file = await fs.promises.stat(filePath);
-        stdout.write(`${path.parse(elem.name).name} - ${path.parse(elem.name).ext.slice(1)} - ${file.size} byte\n`);
-    };
+  try {
+    const folderContent = await fs.promises.readdir(folderPath, {withFileTypes: true});
+    for (const elem of folderContent) {
+      if (!elem.isDirectory()) {
+          const filePath = path.join(folderPath, elem.name);
+          const file = await fs.promises.stat(filePath);
+          stdout.write(`${path.parse(elem.name).name} - ${path.parse(elem.name).ext.slice(1)} - ${file.size} byte\n`);
+      };
+    }
+  } catch (err) {
+    console.log(err);
   }
 }
-
 outputElements()
 
 
